b93fa743a1da107771dee83410cca845439c9ba5
Diclophis <diclophis@gmail.com>
2011-10-27 00:28:58 -0700

    smoothest yet

--- src/SpaceShipDown.cpp
+++ src/SpaceShipDown.cpp
@@ -8,63 +8,60 @@
 SpaceShipDown::SpaceShipDown(int w, int h, std::vector<GLuint> &t, std::vector<foo*> &m, std::vector<foo*> &l, std::vector<foo*> &s) : Engine(w, h, t, m, l, s) {
   LoadSound(2);
   m_IsPushingAudio = true;
-  m_Zoom = 1.0;
+  m_Zoom = 3.0;
 
   m_LandscapeIndex = m_SpriteCount;
   m_AtlasSprites.push_back(new SpriteGun(m_Textures->at(4), 1, 1, 0, 64, 1.0, "", 0, 64, 0.0, 1024, 1024));
   m_AtlasSprites[m_LandscapeIndex]->Build(0);
-  m_AtlasSprites[m_LandscapeIndex]->SetPosition(0.0, 0.0);
+  m_AtlasSprites[m_LandscapeIndex]->SetPosition(0.0, 512.0);
   m_SpriteCount++;
 
   m_PlayerIndex = m_SpriteCount;
-  m_AtlasSprites.push_back(new SpriteGun(m_Textures->at(1), 1, 1, 0, 64, 1.0, "", 0, 64, 0.0, 50, 50));
+  m_AtlasSprites.push_back(new SpriteGun(m_Textures->at(1), 1, 1, 0, 64, 1.0, "", 0, 64, 0.0, 256.0, 256.0));
   m_AtlasSprites[m_PlayerIndex]->Build(0);
-  m_AtlasSprites[m_PlayerIndex]->SetPosition(0.0, 0.0);
+  m_AtlasSprites[m_PlayerIndex]->SetPosition(0.0, 1024.0);
   m_SpriteCount++;
 
   CreateBox2DWorld();
 
-  /*
-  b2BodyDef spriteBodyDef;
-  spriteBodyDef.type = b2_dynamicBody;
-  spriteBodyDef.position.Set(m_AtlasSprites[m_PlayerIndex]->position[0] / PTM_RATIO, m_AtlasSprites[m_PlayerIndex]->position[0] / PTM_RATIO);
-  b2Body *spriteBody = world->CreateBody(&spriteBodyDef);
-  b2PolygonShape spriteShape;
-  spriteShape.SetAsBox(100.0 / PTM_RATIO / 2, 100.0 / PTM_RATIO / 2);
-  b2FixtureDef spriteShapeDef;
-  spriteShapeDef.shape = &spriteShape;
-  spriteShapeDef.density = 10.0;
-  spriteShapeDef.isSensor = true;
-  spriteBody->CreateFixture(&spriteShapeDef);
-  */
-
-  float radius = 5;
-  MLPoint startPosition = MLPointMake(m_AtlasSprites[m_PlayerIndex]->m_Position[0], m_AtlasSprites[m_PlayerIndex]->m_Position[1]);
+  float radius = 128.0;
+  MLPoint startPosition = MLPointMake(m_AtlasSprites[m_PlayerIndex]->m_Position[0] / PTM_RATIO, m_AtlasSprites[m_PlayerIndex]->m_Position[1] / PTM_RATIO);
   b2BodyDef bd;
   bd.type = b2_dynamicBody;
   bd.linearDamping = 0.0f;
   bd.fixedRotation = true;
+  bd.position.Set(startPosition.x, startPosition.y);
   m_PlayerBody = world->CreateBody(&bd);
   b2CircleShape shape;
   shape.m_radius = radius / PTM_RATIO;
   b2FixtureDef fd;
   fd.shape = &shape;
-  fd.density = 2.0f;
+  fd.density = 1.0f;
   fd.restitution = 0.0;
   fd.friction = 0.0;
   m_PlayerBody->CreateFixture(&fd);
   m_PlayerBody->SetActive(true);
 
-  float desiredAngle = atan2f(1.0, 1.0);
-  m_PlayerBody->SetTransform(m_PlayerBody->GetPosition(), desiredAngle);
+  //float desiredAngle = atan2f(1.0, 1.0);
+  //m_PlayerBody->SetTransform(m_PlayerBody->GetPosition(), desiredAngle);
 
-/*
-  b2PolygonShape polygonShape;
-  bd.type = b2_staticBody;
-  bd.position.Set(0, 0);
-  polygonShape.SetAsBox(10.0 / PTM_RATIO, 10.0 / PTM_RATIO);
-  world->CreateBody(&bd)->CreateFixture(&fd);
-*/
+  float floor_size = 512.0;
+  float floor_height = 1.0;
+  // Define the ground body.
+  b2BodyDef groundBodyDef;
+  groundBodyDef.position.Set(0.0, -floor_height / PTM_RATIO);// bottom-left corner
+  
+  // Call the body factory which allocates memory for the ground body
+  // from a pool and creates the ground box shape (also from a pool).
+  // The body is also added to the world.
+  b2Body* groundBody = world->CreateBody(&groundBodyDef);
+  
+  // Define the ground box shape.
+  b2PolygonShape groundBox;   
+  
+  // bottom
+  groundBox.SetAsBox(floor_size / PTM_RATIO, floor_height / PTM_RATIO);
+  groundBody->CreateFixture(&groundBox,0);
 
   m_TouchedLeft = false;
   m_TouchedRight = false;
@@ -111,11 +108,13 @@ int SpaceShipDown::Simulate() {
   b2Vec2 forcePosition = m_PlayerBody->GetWorldCenter();
 
   if (m_TouchedLeft) {
-    m_PlayerBody->ApplyForce(b2Vec2(-0.5, 5), forcePosition);
+    //m_PlayerBody->SetActive(true);
+    m_PlayerBody->ApplyForce(b2Vec2(-600.0, 1500.0), forcePosition);
   }
 
   if (m_TouchedRight) {
-    m_PlayerBody->ApplyForce(b2Vec2(0.5, 5), forcePosition);
+    //m_PlayerBody->SetActive(true);
+    m_PlayerBody->ApplyForce(b2Vec2(600.0, 1500.0), forcePosition);
   }
 
   float x = m_PlayerBody->GetPosition().x * PTM_RATIO;
