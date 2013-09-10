23b245d62a7c61255843c6ec402ff7e12068cc62
Diclophis <diclophis@gmail.com>
2013-09-10 00:30:17 -0700

    repair android

--- bin/android_debug.sh
+++ bin/android_debug.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-rm assets/.DS_Store
-rm assets/models/.DS_Store
-rm assets/textures/.DS_Store
+#rm assets/.DS_Store
+#rm assets/models/.DS_Store
+#rm assets/textures/.DS_Store
 
-cd platforms/android
+#cd platforms/android
 
-~/android-ndk-r4-crystax/ndk-build clean && ~/android-ndk-r4-crystax/ndk-build && \
-ant debug && ant uninstall && ant install
+#~/android-ndk-r4-crystax/ndk-build clean && ~/android-ndk-r4-crystax/ndk-build && \
+#ant debug && ant uninstall && ant install
 
 #~/android-ndk-r4-crystax/ndk-build
 #ant debug && ant uninstall && ant install
+
+cd platforms/android
+
+CRYSTAX=~/android-ndk-r8-crystax-1
+
+#$CRYSTAX/ndk-build clean && 
+$CRYSTAX/ndk-build && ant debug && ant debug uninstall && ant debug install
